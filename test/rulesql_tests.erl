f42f9fd7b49d00ece68e49ed7531e5567306e326
某文 <zhongwencool@gmail.com>
2023-04-23 14:10:02 +0800

    feat: check if string is utf8 string

--- test/rulesql_tests.erl
+++ test/rulesql_tests.erl
@@ -10,6 +10,19 @@
 keyword_1_test() ->
     [?assert(rulesql:is_reserved(Key)) || Key <- ?RESERVED_KEYS].
 
+utf8_test_() ->
+    [
+      ?_assertMatch(
+        {parse_error, {invalid_utf8, _}},
+        rulesql:parsetree(<<"SELECT x FROM \"测试专用topic\"">>)),
+      ?_assertMatch(
+        {ok,{select,
+          [{fields,['*']},
+            {from,[<<"测试专用topic"/utf8>>]},
+            {where,{}}]}},
+        rulesql:parsetree(<<"SELECT * FROM \"测试专用topic\""/utf8>>))
+    ].
+
 select_test_() ->
     [
         %% basic select
